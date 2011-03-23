f83bacea20d426267ed0781a67a2dd8293c1a859
Mark Karpeles <mark@tibanne.com>
2011-03-23 01:03:32 +0000

    missing degica profile file

---
+++ includes/profiles/degica.h
@@ -0,0 +1,6 @@
+#define APPLICATION_COMPANY "Degica"
+#define APPLICATION_COMPANY_DOMAIN "degica.com"
+#define APPLICATION_NAME "DegicaTemplatesEditor"
+
+#define WINDOW_ICON "degica.ico"
+#define WINDOW_LOGO "degica.png"
